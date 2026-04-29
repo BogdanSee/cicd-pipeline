6a5b9a5e7d2e2a74d8def99544b715f20a503bcc
BogdanSee <sereabogdan27@gmail.com>
2026-04-30 01:06:52 +0300

    Add power function

--- app.py
+++ app.py
@@ -18,3 +18,6 @@ if __name__ == "__main__":
     print(f"10 - 4 = {subtract(10, 4)}")
     print(f"3 x 4 = {multiply(3, 4)}")
     print(f"10 / 2 = {divide(10, 2)}")
+
+if power(a,b):
+   return a**b
